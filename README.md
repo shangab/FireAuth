38f393fea2aafe7d16937671443bfbc269fa7f5e
Abubaker Shangab <shangab@gmail.com>
2015-05-03 03:25:30 -0500

    First upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # FireAuth 
+
+<h1>How to use it:</h1>
+         <ul>
+                <li>Download the project from GitHub</li>
+                <li>in js/app.js replace fireBaseUrl with your own Firebase Application</li>
+                <li>Done</li>
+                
+            </ul>
--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var app = angular.module('authApp', ['firebase']);
-
+var fireBaseUrl='https://shangab.firebaseio.com/';
 app.controller('Ctrl', function ($scope, $firebaseArray) {
-    var ref = new Firebase('https://shangab.firebaseio.com/users');
+    var ref = new Firebase(fireBaseUrl+ 'users');
     $scope.users = $firebaseArray(ref);
     
     $scope.loginFacebook = function () {
-        var ref = new Firebase("https://shangab.firebaseio.com");
+        var ref = new Firebase(fireBaseUrl);
         ref.authWithOAuthPopup("facebook", function (error, authData) {
             if (error) {
                 console.log("Login Failed!", error);
@@ -18,7 +18,7 @@ app.controller('Ctrl', function ($scope, $firebaseArray) {
         });
     };
     $scope.loginGithub = function () {
-        var ref = new Firebase("https://shangab.firebaseio.com");
+        var ref = new Firebase(fireBaseUrl);
         ref.authWithOAuthPopup("github", function (error, authData) {
             if (error) {
                 console.log("Login Failed!", error);
@@ -30,7 +30,7 @@ app.controller('Ctrl', function ($scope, $firebaseArray) {
         });
     };
     $scope.loginGoogle = function () {
-        var ref = new Firebase("https://shangab.firebaseio.com");
+        var ref = new Firebase(fireBaseUrl);
         ref.authWithOAuthPopup("google", function (error, authData) {
             if (error) {
                 console.log("Login Failed!", error);
@@ -42,7 +42,7 @@ app.controller('Ctrl', function ($scope, $firebaseArray) {
         });
     };
     $scope.loginTwitter = function () {
-        var ref = new Firebase("https://shangab.firebaseio.com");
+        var ref = new Firebase(fireBaseUrl);
         ref.authWithOAuthPopup("twitter", function (error, authData) {
             if (error) {
                 console.log("Login Failed!", error);
